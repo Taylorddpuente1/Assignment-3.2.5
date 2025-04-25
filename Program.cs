af368c180a3005f7ba17ba852a99b9dcf3e6561a
Taylo <Taylo@MARIEL>
2025-04-24 19:14:04 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MyApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNums = { 3, 4, 5, 2, 7, 8, 9,0 };
+            int target = 7;
+           int targetIndex = FindIndex(myNums, target);
+            Console.WriteLine($"For target {myNums[targetIndex]} the index is:{targetIndex}. ");
+
+
+
+           static int FindIndex(int[] Nums, int target)
+            {
+                for (int i = 0; i < Nums.Length; i++)
+                {
+                    if (Nums[i] == target)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+        }
+    }
+}
